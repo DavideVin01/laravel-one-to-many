49c13372982a7229b0f0f5de9307100ac30a49f2
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-29 20:55:12 +0200

    Latest changes

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -14,8 +16,10 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $category_ids = Category::pluck('id')->toArray();
         for ($i = 0; $i < 50; $i++) {
             $post = new Post();
+            $post->category_id = Arr::random($category_ids);
             $post->title = $faker->text(20);
             $post->content = $faker->paragraph(10, false);
             $post->image = $faker->imageUrl(250, 250);
